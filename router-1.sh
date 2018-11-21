ac9b3c9c06faee47373d977dd854b0e94172bc57
Unknown <daniele.mattedi@studenti.unitn.it>
2018-11-21 10:12:29 +0100

    aggiunto frr

--- router-1.sh
+++ router-1.sh
@@ -10,6 +10,25 @@ ip link add link eth1 name eth1.170 type vlan id 170
 ip link add link eth1 name eth1.171 type vlan id 171
 ip add add 192.168.170.254/24 dev eth1.170
 ip add add 192.168.171.254/27 dev eth1.171
+
 ip link set eth1.170 up
 ip link set eth1.171 up
+ip link set dev eth2 up
+ip add add 192.168.173.1/30 dev eth2
+ip link set eth2 up
 sysctl net.ipv4.ip_forward=1
+
+sed -i "s/\(zebra *= *\). */\1yes/" /etc/frr/daemons
+sed -i "s/\(ospfd *= *\). */\1yes/" /etc/frr/daemons
+service frr restart
+vtysh
+vtysh conf t
+vtysh router ospf
+vtysh redistribute connected
+vtysh exit
+vtysh interface eth2
+vtysh ip ospf area 0.0.0.0
+vtysh exit 
+vtysh write
+
+
--- router-2.sh
+++ router-2.sh
@@ -8,3 +8,19 @@ apt-get install -y frr --assume-yes --force-yes
 ip link set dev eth1 up
 ip add add 192.168.172.230/30 dev eth1
 ip link set eth1 up
+ip link set dev eht2 up
+ip add add 192.168.173.2/30 dev eth2
+ip link set eth2 up
+sysctl net.ipv4.ip_forward=1
+sed -i "s/\(zebra *= *\). */\1yes/" /etc/frr/daemons
+sed -i "s/\(ospfd *= *\). */\1yes/" /etc/frr/daemons
+service frr restart
+vtysh
+vtysh conf t
+vtysh router ospf
+vtysh redistribute connected
+vtysh exit
+vtysh interface eth2
+vtysh ip ospf area 0.0.0.0
+vtysh exit 
+vtysh write
