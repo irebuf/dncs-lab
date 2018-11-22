234ddf31cb8e0c6cf671c72931293a17b62b89b0
Unknown <daniele.mattedi@studenti.unitn.it>
2018-11-22 12:19:28 +0100

    modifiche3

--- docker-2c.sh
+++ docker-2c.sh
@@ -1,22 +1,22 @@
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+
+apt-get remove docker docker-engine docker.io
+apt-get update --assume-yes --force-yes
+apt-get install apt-transport-https --assume-yes --force-yes
+apt-get install ca-certificates --assume-yes --force-yes
+apt-get install curl --assume-yes --force-yes
+apt-get install software-properties-common --assume-yes --force-yes
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce --assume-yes --force-yes
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+
 
 ip link set dev eth1 up
 ip add add 192.168.172.229/30 dev eth1
 ip route add 192.168.168.0/21 via 192.168.172.230
 
-apt-get remove docker docker-engine docker.io
-apt-get update
-apt-get install \ apt-transport-https \ ca-certificates \ curl \ software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce  --assume-yes --force-yes
 mkdir -p ~/docker-nginx/html
 cd ~/docker-nginx/html
 touch index.html
