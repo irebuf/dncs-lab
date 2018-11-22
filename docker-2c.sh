390b9a3b21afba2ea93c9d434bbb8ee29090934f
Irene Buffa <irene.buffa@studenti.unitn.it>
2018-11-22 09:18:54 +0100

    sistemo 2-c

--- docker-2c.sh
+++ docker-2c.sh
@@ -1,9 +1,14 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce --assume-yes --force-yes
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
 
 ip link set dev eth1 up
 ip add add 192.168.172.229/30 dev eth1
-ip route add 192.168.172.228/30 via 192.168.172.230
+ip route add 192.168.168.0/21 via 192.168.172.230
 
 apt-get remove docker docker-engine docker.io
 apt-get update
