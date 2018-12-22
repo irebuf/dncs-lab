d68a4c68d9745549c88cfe0b7aec1730ce6295a6
Unknown <daniele.mattedi@studenti.unitn.it>
2018-12-22 14:42:19 +0100

    aggiunto curl in host1-a

--- common.sh
+++ common.sh
@@ -1,5 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get update --assume-yes --force-yes
+apt-get install apt-transport-https --assume-yes --force-yes
+apt-get install ca-certificates --assume-yes --force-yes
+apt-get install curl --assume-yes --force-yes
+apt-get install software-properties-common --assume-yes --force-yes
 apt-get install -y tcpdump --assume-yes
 ip link set dev eth1 up
 ip add add 192.168.170.1/24 dev eth1
